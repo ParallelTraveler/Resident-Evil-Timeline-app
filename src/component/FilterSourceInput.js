878809371433084de1be66a11edeaf2d63303dbd
Alex Taylor <alex.m.taylor@gmail.com>
2022-09-15 16:48:36 +0100

    Added ability to select all in the source filter.

--- src/component/FilterSourceInput.js
+++ src/component/FilterSourceInput.js
@@ -1,28 +1,65 @@
 import * as React from 'react';
 import FormControl from '@mui/material/FormControl';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
-import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
+import { withStyles } from "@mui/material";
+
+const styles = (theme) => ({
+  root: {
+  }
+}); 
 
 export default class FilterSourceInput extends React.Component {
+  isAllSelected = false;
+
   constructor(props) {
     super(props);
     this.state = { value: this.props.options };
     this.handleChange = this.handleChange.bind(this);
+    this.isAllSelected = this.props.options.length > 0 && this.state.value.length === this.props.options.length;
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
-    this.props.handleInput('filter_source', event.target.value);
+    const value = event.target.value;
+    const selected = this.state.value;
+    const options = this.props.options;
+    if (value[value.length - 1] === 'all') {
+      this.setState({ value: selected.length === options.length ? [] : options});
+      return;
+    }
+    this.setState({ value: value });
+    this.props.handleInput('filter_source', value);
   }
 
   getOptions() {
     let items = [];
-    for (let i = 0; i < this.props.options.length; i++) {
-      let value = this.props.options[i];
+    const selected = this.state.value;
+    const options = this.props.options;
+    this.isAllSelected = options.length > 0 && selected.length === options.length;
+    items.push(
+      <MenuItem
+        value="all"
+      >
+        <ListItemIcon>
+          <Checkbox
+            checked={this.isAllSelected}
+            indeterminate={
+              selected.length > 0 && selected.length < options.length
+            }
+          />
+        </ListItemIcon>
+        <ListItemText
+          primary="Select all"
+        />
+      </MenuItem>
+    );
+    for (let i = 0; i < options.length; i++) {
+      let value = options[i];
       items.push(
         <MenuItem key={value} value={value}>
           <Checkbox checked={this.state.value.indexOf(value) > -1} />
